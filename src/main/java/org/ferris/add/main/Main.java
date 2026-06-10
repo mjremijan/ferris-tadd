c59540a81ca2b904babb9ef9e8aa85c2af0ee122
Michael Remijan <mjremijan@yahoo.com>
2026-06-09 21:11:07 -0500

    sort by string length then sort alphabetical print to stdout Use tables in html Add show summary to html start method to get api key

--- src/main/java/org/ferris/add/main/Main.java
+++ src/main/java/org/ferris/add/main/Main.java
@@ -2,7 +2,6 @@ package org.ferris.add.main;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URI;
@@ -21,6 +20,7 @@ public class Main {
         TvMazeClient client = new TvMazeClient();
         TvMazeParser parser = new TvMazeParser();
 
+        String json = client.downloadSchedule();
 
 //        try (InputStream in = client.downloadSchedule()) {
 //            AtomicInteger i = new AtomicInteger(1);
@@ -55,57 +55,82 @@ public class Main {
         DateTimeFormatter formatter
             = DateTimeFormatter.ofPattern("EEEE, MMMM d");
         
+        parser.streamUpcomingEpisodes(json)
+            .collect(Collectors.groupingBy(
+                a -> a.episode().airDate(),
+                TreeMap::new,
+                Collectors.toList())
+            ).forEach((date, episodesForDate) -> {
+
+                System.out.printf("%n%s%n%n", date.format(formatter));
+
+                record R(String channel, String show){};
+                episodesForDate.stream()
+                    .map(a -> new R(a.channel().name(), a.show().name()))
+                    .sorted(Comparator.comparingInt((R r) -> r.channel().length())
+                            .thenComparing(R::channel)
+                    )
+                    .distinct()
+                    .forEach(r -> System.out.printf("  [%s]  %s%n", r.channel(), r.show()))
+                ;
+            });  
+                
+        
+        
+        
         StringWriter sw = new StringWriter();
         PrintWriter out = new PrintWriter(sw);
                 
-        try (InputStream in = client.downloadSchedule()) {
-            parser.streamUpcomingEpisodes(in)
-                .collect(Collectors.groupingBy(
-                    a -> a.episode().airDate(),
-                    TreeMap::new,
-                    Collectors.toList())
-                ).forEach((date, episodesForDate) -> {
+        parser.streamUpcomingEpisodes(json)
+            .collect(Collectors.groupingBy(
+                a -> a.episode().airDate(),
+                TreeMap::new,
+                Collectors.toList())
+            ).forEach((date, episodesForDate) -> {
 
-                    out.printf("<p>%s</p>", date.format(formatter));
+                out.printf("<p><b>%s</b></p>", date.format(formatter));
 
-                    record r(String channel, String show){};
-                    out.printf("<p>%n");
-                    episodesForDate.stream()
-                        .map(a -> new r(a.channel().name(), a.show().name()))
-                        .sorted(Comparator.comparing(r -> r.channel().length()))
-                        .distinct()
-                        .forEach(r -> out.printf("&nbsp;&nbsp;[%s]  %s<br/>", r.channel(), r.show()))
-                    ;
-                    out.printf("</p>");
-                });  
-            System.out.println(sw.toString());
-            
-            
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectNode req = mapper.createObjectNode();
-            req.put("from", "onboarding@resend.dev");
-            req.putArray("to").add("mjremijan@yahoo.com");
-            req.put("subject", "TV Digest for today");
-            req.put("html", sw.toString());
-            String body = mapper.writeValueAsString(req);
-            
-            HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.resend.com/emails"))
-                .header("Authorization", "Bearer xxxxxx")
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(body))
-                .build();
-            
-            HttpClient emailClient = HttpClient.newHttpClient();
-            HttpResponse<String> response =
-                emailClient.send(
-                    request,
-                    HttpResponse.BodyHandlers.ofString()
-                );
+                record R(String channel, String show, String summary){}
+                out.printf("<table border=\"1\">%n");
+                episodesForDate.stream()
+                    .map(a -> new R(a.channel().name(), a.show().name(), a.show().summary()))
+                    .sorted(Comparator.comparingInt((R r) -> r.channel().length())
+                            .thenComparing(R::channel)
+                    )
+                    .distinct()
+                    .forEach(r -> out.printf("<tr><td style=\"padding: 10px;\">[%s]</td><td style=\"padding: 10px;\"><b>%s</b><br />%s</td></tr>", r.channel(), r.show(), r.summary()))
+                ;
+                out.printf("</table>");
+            });  
 
-            System.out.println(response.statusCode());
-            System.out.println(response.body());
-            
-        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode req = mapper.createObjectNode();
+        req.put("from", "onboarding@resend.dev");
+        req.putArray("to").add("mjremijan@yahoo.com");
+        req.put("subject", "TV Digest for today");
+        req.put("html", sw.toString());
+        String body = mapper.writeValueAsString(req);
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://api.resend.com/emails"))
+            .header("Authorization", "Bearer " + getApiKey())
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(body))
+            .build();
+
+        HttpClient emailClient = HttpClient.newHttpClient();
+        HttpResponse<String> response =
+            emailClient.send(
+                request,
+                HttpResponse.BodyHandlers.ofString()
+            );
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+
+    private static String getApiKey() {
+        return "";
     }
 }
