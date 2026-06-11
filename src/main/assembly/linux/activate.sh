18a68f13917f9f6c7527e59388257d281adebca5
Michael Remijan <mjremijan@yahoo.com>
2026-06-11 13:46:42 -0500

    fix problem with "Remaking active"

--- src/main/assembly/linux/activate.sh
+++ src/main/assembly/linux/activate.sh
@@ -1,40 +1,40 @@
-#!/bin/bash                                                                                                                                        
-                                                                                                                                                   
-if (( $# != 1))                                                                                                                                    
-then                                                                                                                                               
-        echo "Usage active.sh [version]"                                                                                                           
-        exit 1                                                                                                                                     
-fi                                                                                                                                                 
-                                                                                                                                                   
-BASE=ferris-tadd-$1-prod                                                                                                                           
-                                                                                                                                                   
-if [ ! -s "$BASE.tar.gz" ]; then                                                                                                                   
-        echo "File does not exist: $BASE.tar.gz"                                                                                                   
-        exit 1                                                                                                                                     
-fi                                                                                                                                                 
-                                                                                                                                                   
-if [ -d "$BASE" ]; then                                                                                                                            
-        echo "Deleting directory $BASE"                                                                                                            
-                rm -rf $BASE                                                                                                                       
-fi                                                                                                                                                 
-                                                                                                                                                   
-tar xvzf $BASE.tar.gz                                                                                                                              
-                                                                                                                                                   
-                                                                                                                                                   
-#####################                                                                                                                              
-## CONF                                                                                                                                            
-#####################                                                                                                                              
-cd $BASE                                                                                                                                           
-cd conf                                                                                                                                            
-                                                                                                                                                   
-echo "Linking to properties"                                                                                                                            
-rm -f tadd.properties                                                                                                                              
-ln -s ../../tadd.properties                                                                                                                        
-                                                                                                                                                   
-####################                                                                                                                               
-## ACTIVE                                                                                                                                          
-####################                                                                                                                               
-cd ..                                                                                                                                              
-echo "Remaking active"                                                                                                                             
-rm active                                                                                                                                          
+#!/bin/bash
+
+if (( $# != 1))
+then
+    echo "Usage active.sh [version]"
+    exit 1
+fi
+
+BASE=ferris-tadd-$1-prod
+
+if [ ! -s "$BASE.tar.gz" ]; then
+    echo "File does not exist: $BASE.tar.gz"
+    exit 1
+fi
+
+if [ -d "$BASE" ]; then
+    echo "Deleting directory $BASE"
+    rm -rf $BASE
+fi
+
+tar xvzf $BASE.tar.gz
+
+#####################
+## CONF
+#####################
+cd $BASE
+cd conf
+
+echo "Linking to properties"
+rm -f tadd.properties
+ln -s ../../tadd.properties
+
+####################
+## ACTIVE
+####################
+cd ..
+cd ..
+echo "Remaking active"
+rm active
 ln -s $BASE active
